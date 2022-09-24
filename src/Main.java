f1cbbd85025a13e47cb83142973a862c95ef1f95
Agureeva Anna <agureeva@ro.ru>
2022-09-25 02:58:26 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        WordsChecker text = new WordsChecker("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+        System.out.println(text.hasWord("ali"));
+        System.out.println(text.hasWord("Lorem"));
+        System.out.println(text.hasWord("adipiscing"));
+        System.out.println(text.hasWord("lorem"));
+        System.out.println(text.hasWord("query"));
+        System.out.println(text.hasWord("lor"));
+        System.out.println(text.hasWord("English"));
+    }
+}
